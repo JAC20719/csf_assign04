54939f9b42e4351e44551de27e13d611ec0a40b0
Justin <jcanedy07@gmail.com>
2020-11-09 12:50:41 -0500

    mirrorh

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC = gcc
 CFLAGS = -g -Wall -Wextra -pedantic -std=gnu99 -fPIC
 
-PLUGIN_SRCS = swapbg.c expose.c mirrorv.c
-#^^^^mirrorh.c tile.c^^^^
+PLUGIN_SRCS = swapbg.c expose.c mirrorv.c mirrorh.c
+#^^^^tile.c^^^^
 SRCS = imgproc.cpp pnglite.c $(PLUGIN_SRCS)
 
 OBJS = $(SRCS:%.c=%.o)
---
+++ mirrorh.c
@@ -0,0 +1,78 @@
+#include <stdio.h> //remove
+#include <stdlib.h>
+#include "image_plugin.h"
+
+struct Arguments {
+	// This plugin accepts 1 command line arguments;
+	// factor to multiply each pixel by.
+        int dummy;
+};
+
+const char *get_plugin_name(void) {
+	return "mirrorh";
+}
+
+const char *get_plugin_desc(void) {
+	return "mirror image horizontally";
+}
+
+void *parse_arguments(int num_args, char *args[]) {
+	(void) args; // this is just to avoid a warning about an unused parameter
+    
+	if (num_args != 0) {
+	    return NULL;
+	}
+	
+	return calloc(1, sizeof(struct Arguments));
+}
+
+struct Image *transform_image(struct Image *source, void *arg_data) {
+	struct Arguments *args = arg_data;
+
+	// Allocate a result Image
+	struct Image *out = img_create(source->width, source->height);
+	if (!out) {
+		free(args);
+		return NULL;
+	}
+
+	//Put pixels into out
+	unsigned num_pixels = source->width * source->height;
+	for (unsigned i = 0; i < num_pixels; i++) {
+	  out->data[i] = source->data[i];
+	}
+
+	//Make 2D Array for easier use
+	uint32_t twoD[source->width][source->height];
+	unsigned pixel = 0;
+	for(unsigned i = 0; i < source->height; i++) {
+	  for(unsigned j = 0; j < source->width; j++) {
+	    twoD[j][i] = out->data[pixel];
+	    pixel++;
+	  }
+	}
+
+	//Change the pixels in the 2D Array (Reflect across y)
+	for(unsigned i = 0; i < source->height; i++) {
+	  for(unsigned j = 0; j < source->width / 2; j++) {
+	    uint32_t temp = twoD[j][i];
+	    twoD[j][i] = twoD[source->width - j - 1][i];
+	    twoD[source->width - j - 1][i] = temp;
+	  }
+	}
+
+	//Put pixels back into out
+	pixel = 0;
+	for(unsigned i = 0; i < source->height; i++) {
+	  for(unsigned j = 0; j < source->width; j++) {
+	    out->data[pixel] = twoD[j][i];
+	    pixel++;
+	  }
+	}
+
+	
+
+	free(args);
+
+	return out;
+}
